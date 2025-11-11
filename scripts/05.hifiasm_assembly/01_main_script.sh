4b758058021696f161d770239029ee7486027d00
evadriggs <ejasinskidriggs@calacademy.org>
2025-11-10 16:44:28 -0800

    hard coded main script for hifiasm to access the input file, might need to adjust this later

--- scripts/05.hifiasm_assembly/01_main_script.sh
+++ scripts/05.hifiasm_assembly/01_main_script.sh
@@ -3,6 +3,6 @@
 threads=96
 telo=CCCTAA  # lex lower of TTAGGG vert telo motif
 
-hifi_reads=m84286_250729_002700_s2_GalDove.cln.fq.gz
+hifi_reads="/home/ejasinskidriggs/galapagos_dove_project/genome_dove/GalapaGenomes-Galapagos-Dove/output/03.clean_adapters/m84286_250729_002700_s2_GalDove.cln.fq.gz"
 
 hifiasm.sh -t $threads --telo-m $telo $hifi_reads
